38d09a3007e7399a1633df281231800d423b0bcf
Thomas Nordentoft <thomas@nordentoft.dk>
2026-06-04 17:37:43 +0200

    Make floating

--- frontend/src/components/PageSidebar.tsx
+++ frontend/src/components/PageSidebar.tsx
@@ -10,11 +10,16 @@ const SIDEBAR_WIDTH = 'min(28rem, calc(100vw - 6rem))';
 const PageSidebar = ({ open, onOpenChange }: PageSidebarProps) => (
     <aside
         aria-hidden={!open}
-        className="hidden min-h-0 shrink-0 overflow-hidden border-r border-border bg-background/95 transition-[width] duration-300 ease-out md:block"
+        className="hidden min-h-0 shrink-0 overflow-hidden transition-[width] duration-300 ease-out md:block"
         style={{ width: open ? SIDEBAR_WIDTH : 0 }}
     >
-        <div className="flex h-full w-[min(28rem,calc(100vw-6rem))] min-w-0 flex-col pt-14">
-            <SidebarBrowser className="min-h-0 flex-1 p-3" onClose={() => onOpenChange(false)} />
+        <div className="flex h-full w-[min(28rem,calc(100vw-6rem))] min-w-0 flex-col p-3 pt-17">
+            <div className="bg-background/85 border-border/80 flex min-h-0 flex-1 flex-col overflow-hidden rounded-xl border shadow-sm backdrop-blur-xl">
+                <SidebarBrowser
+                    className="min-h-0 flex-1 p-3"
+                    onClose={() => onOpenChange(false)}
+                />
+            </div>
         </div>
     </aside>
 );
