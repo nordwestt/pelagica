968a48637dbfe5c1e50442f5a01b0ab6a7c994c2
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-06-13 12:35:56 +0200

    Fix logout method clearing device id

--- frontend/src/api/logout.ts
+++ frontend/src/api/logout.ts
@@ -1,9 +1,7 @@
 import { clearCredentials } from '@/utils/localstorageCredentials';
-import { clearDeviceId } from '@/utils/deviceId';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function logout(api: any) {
     clearCredentials();
-    clearDeviceId();
     await api.logout();
 }
